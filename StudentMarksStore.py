990600d8b9acc7ab2e55ce7980e5071b87edd31b
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-06-16 20:38:21 +0530

    for better understanding just print the student name and marks

---
+++ StudentMarksStore.py
@@ -0,0 +1,10 @@
+#in this program we will store the student marks in proper frmat
+
+name="Swapnil"
+maths=80
+eng=30
+marathi=50
+print("Student Name is:",name)
+print("Marks in maths is ",maths)
+print("Marks in english is ",eng)
+print("Marks in marathi is",marathi)
