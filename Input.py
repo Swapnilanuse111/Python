435c5d2bd065fc9872822e311674b35661989511
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-06-18 19:42:23 +0530

    Added Day of learning python

---
+++ Input.py
@@ -0,0 +1,9 @@
+#input function-using input function we can takke input from the usre
+#syntax of input function
+#variable_name=input("message")
+name=input("eneter your name:")
+print("My name is "+name)
+
+x=input("eneter the 1st number:")   #in python the input function take user input as string values
+y=input("enetr the secoend number:")#we need to mention the datatype using type casting 
+print(int(x)+int(y))
---
+++ TypeCasting.py
@@ -0,0 +1,9 @@
+a=1
+b=2
+print(int(a)+int(b))
+
+#Implicit type casting
+c=2
+d=10.2
+print(c+d)
+print(type(c))
