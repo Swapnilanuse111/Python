55a44fb05b310137e8745656e3f00aa1815f5b42
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-06-26 20:42:58 +0530

    Day 13: Practiced Python Data Types, Strings, Variables, and Quote Types

---
+++ 02_DataTypes/String.py
@@ -0,0 +1,7 @@
+# <!-- #I have Creted One String Which Contain String Value
+#   like A to Z Alphabate a to z lowecase and 0 to 9 
+#  values SpecialCharacter like @ #!%7^() like That Type Of 
+#  Character We Store in striing -->
+name='Swapnil'
+print(name)
+ 
---
+++ 02_DataTypes/TypeOfQuote.py
@@ -0,0 +1,17 @@
+# In Python, there are three types of quotes:
+# 1. Single Quote
+# 2. Double Quote
+# 3. Triple Quote
+
+# 1. Single Quote    --->it is most commanly used for simple text
+name = 'swapnildeveloper'
+print(name)
+
+# 2. Double Quote
+name = "rahulfarmasist"
+print(name)
+
+# 3. Triple Quote
+name = '''Manoj Chandavle is the owner of Maha E-Seva Kendra,
+Shakha Atpadi.'''
+print(name)
