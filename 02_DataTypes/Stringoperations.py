0c2c8dc5db2ba728dfa2eb4afdfc4c35e640abd3
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-06-27 22:09:45 +0530

    Day 14: Practiced Python String Operations and Built-in String Methods

---
+++ 02_DataTypes/Stringoperations.py
@@ -0,0 +1,21 @@
+#1]Concatination Opreation
+a="Swapnil"
+b="Rahul"
+print(a+b) #This is concatination process how to muarj two diffrent sting into one formate
+
+#2]Repeatation Of String 
+a="SOPAN LIVE    "
+print(a*5)  #using * we are print the String As Multiple Time 
+
+
+#3]UpperCase Charater 
+a="rahul"
+print(a.upper()) #using upper function we print the String as  upper case
+
+#4]Lower Case Character String
+a="AnUse"
+print(a.lower()) # Usiing the lower() function we are print the all string as lowercase
+
+#5]Captlize 
+a="Rahul"
+print(a.capitalize())
