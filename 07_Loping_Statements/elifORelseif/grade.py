2566fd6ba482f660156d76a04c905cf4ed3abe60
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-07-14 20:04:51 +0530

    Day 30: Practiced Python Conditional Statements with Grade, Number Validation, and Login Programs

---
+++ 07_Loping_Statements/elifORelseif/grade.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter The Marks\n"))
+if marks>=90:
+    print("YOU GET A GRADE")
+elif marks>=75:
+    print("YOU GET B GRAD")
+elif marks>=50:
+    print("YOU GATE C GRADE")
+elif marks>=35:
+    print("YOU ARE PASS")
+else:
+    print("YOU ARE FAIL")
---
+++ 07_Loping_Statements/nestedIF/checktheLOGINDETAIL.py
@@ -0,0 +1,9 @@
+username="rahulanuse"
+password="1234"
+if username=="rahulanuse":
+    if password=="1234":
+        print("YOU ARE LOGIN SUCESSFULLY")
+    else:
+        pritn("Password is wrong")
+else:
+    print("Username Is No Correct")
