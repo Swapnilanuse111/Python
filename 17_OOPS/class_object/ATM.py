660336d894396b96c5bb990554e7d85276bf6d70
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-08-23 20:31:14 +0530

    Day 66:OOPs Revision Done

--- 17_OOPS/class_object/ATM.py
+++ 17_OOPS/class_object/ATM.py
@@ -1,15 +1,25 @@
 class ATM:
-
     def __init__(self):
-        self.c_name = input("Enter The Customer Name")
-        self.c_ac_no =input("Enter The Customer Account Number")
-        self.c_bal=int(input("Enter The Bank Balance"))
+        self.c_name = input("Enter The Customer Name:-")
+        self.c_ac_no =input("Enter The Customer Account Number:-")
+        self.c_bal=2000
     
     def Deposite(self):
-        self.depo=int(input("Enter The Amount To Deposite"))
-        self.c_bal=self.c_bal+self.depo
-        print("Now Yore Bank Balance Is",self.c_bal)
+        self.depo=int(input("Enter The Amount To Deposite:-"))
+        if self.depo >= 100:
+            self.c_bal=self.c_bal+self.depo
+            print("Now Yore Bank Balance Is:-",self.c_bal)
+        else:
+            print("Youre Ammount Is Not Suficient For Add")
+
+    def Withdrow(self):
+        self.Withdrow=int(input("Enter The Amoount How Manny Withdrow:-"))
+        if self.Withdrow<self.c_bal:
+            self.Withdrow=self.c_bal-self.Withdrow
+            print("Your Money Withdrow Sucessfully")
+            print("Avalable Balance is:-",self.Withdrow)
+        else:
+            print("In Youre Bank Account There Is No Suficent Balance")
 obj=ATM()
 obj.Deposite()
-
-
+obj.Withdrow()
