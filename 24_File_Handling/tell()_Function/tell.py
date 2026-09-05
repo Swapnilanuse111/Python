b809a4f5e01431958bcbcb3bfe7d61305bf08961
Swapnil Anuse <swapnilanuse111@gmail.com>
2026-09-05 18:00:00 +0530

    Day 79: Practiced DSA Tree Operations

---
+++ 24_File_Handling/tell()_Function/tell.py
@@ -0,0 +1,7 @@
+'''tell() function?
+---->It is a type of function which is used to find the exact position of cursor where the cursor is preesent
+
+
+'''
+f=open("data.txt","r")
+print(f.tell())
---
+++ 24_File_Handling/withkeywordAppend_function.py
@@ -0,0 +1,5 @@
+with open("data.txt","a+") as f:
+    add="Ramdas Patil"
+    f.write(add)
+    a=f.read()
+    print(a)
